b3aa863c6eaf6e5d4fca69beac06d962e3615d52
AsadAli-1122 <asadali.cool.23@gmail.com>
2023-02-23 17:50:29 +0500

    werter

--- src/assets/pages/Dashboard.js
+++ src/assets/pages/Dashboard.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import Button from '../components/Button'
-// import Button from './Button'
 
 
 
@@ -23,17 +22,17 @@ export default function Dashboard() {
 
     
     const [text, setText] = useState('');
-  return (
-    <div>
-      <div className="pt-20 md:ml-64 min-h-[100vh] bg-dark-gray">
+return (
+    <>
+    <div className="pt-20 md:ml-64 min-h-[100vh] bg-dark-gray">
         <div className="p-4">
             
-        <div className="flex items-center justify-center mb-4 py-4 rounded bg-gray-50 bg-opacity-10">
+        <div className="flex items-center justify-center mb-4 p-4 rounded bg-gray-50 bg-opacity-10">
             <p className="text-2xl text-syellow" >Dashboard</p>
         </div>
         
         <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 bg-opacity-10">
-            <textarea name="subject" id="subject" cols="" rows="" className='w-full h-full rounded bg-transparent p-6 border-gray-600 focus:ring-0 focus:outline-none focus:border focus:border-syellow text-gray-400' placeholder="Write and convert something here." value={text} onChange={OnChnage}></textarea>
+            <textarea name="subject" id="subject" cols="" rows="" className='w-full h-full rounded bg-transparent p-6 border-gray-600 focus:ring-0 focus:outline-none focus:border focus:border-syellow text-gray-400 overflow-y-scroll resize-none' placeholder="Write and convert something here." value={text} onChange={OnChnage} spellCheck={false}></textarea>
         </div>
         <div className='flex flex-wrap justify-center items-center mb-4'>
             <div onClick={UpperCase}>
@@ -46,9 +45,23 @@ export default function Dashboard() {
             <Button btnText='Remove'/>
             </div>
         </div>
+        <div className="mb-4 p-4 rounded bg-gray-50 bg-opacity-10 text-white">
+            <h1 className='text-3xl text-center text-syellow font-bold '>Summary</h1>
+            <p>
+                <span className='inline-block w-12'>{text.split(" ").length-1}</span>Words<br />
+                <span className='inline-block w-12'>{text.length}</span>Characters<br />
+                <span className='inline-block w-12'>{(0.008 * text.split(" ").length).toFixed(3)}</span>Minutes time takes to read this.
+            </p>
+        </div>
+        <div className="mb-4 p-4 rounded bg-gray-50 bg-opacity-10 text-white">
+            <h1 className='text-3xl text-center text-syellow font-bold '>Preview</h1>
+            <p className='text-base text-gray-300'>
+                {text}
+            </p>
+        </div>
         
         </div>
     </div>
-    </div>
+    </>
   )
 }
